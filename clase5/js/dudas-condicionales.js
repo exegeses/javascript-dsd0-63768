a29d3ac53f8d20c2f7d04d65aaf12da451eebbc8
Marcos <exegeses@gmail.com>
2022-12-26 14:10:14 -0300

    respuesta a dudas de condicionales y operador ==

---
+++ clase5/js/dudas-condicionales.js
@@ -0,0 +1,12 @@
+// el = (uno sólo) es el operador de ASIGNACIÖN
+let nombre = 'Marcos';
+
+
+/* en la paréntesis del if
+    si queremos COMPARAR el valos
+    de una variable, debemos usar 
+    el == ( doble ) es el operador de COMPARACIÓN
+*/
+if( nombre == 'Admin' ){
+    document.write('bienvenido Admin');
+}
